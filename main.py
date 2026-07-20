1baf424823d226ff9f1d8d300fa0f266063ec22e
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-07-20 17:30:19 -0500

    Fixed a lot of bugs

--- main.py
+++ main.py
@@ -9,47 +9,45 @@ history = []
 continue_or_not = True
 
 while continue_or_not == True:    
-    if counter == 0:
-        operation = input("What feature are you going to do? (Addition, Subtraction, Multiplication, Division, Modulus, View History): ")
-    
-    elif counter > 0:
-        operation = input("You typed in an invalid operation. Try again: ")
-        
-    num1, num2 = check_num()
+    operation = input("What feature are you going to do? (Addition, Subtraction, Multiplication, Division, Modulus, View History, Delete History): ")
     
     operation = operation.lower()
 
     if operation == "addition" or operation == "add":
+        num1, num2 = check_num()
         addition(num1, num2)
         history.append(f"{num1} + {num2} = {num1 + num2}")
-        counter = 0
      
     elif operation == "subtraction" or operation == "subtract":
+        num1, num2 = check_num()
         subtraction(num1, num2)
         history.append(f"{num1} - {num2} = {num1 - num2}")
-        counter = 0
-        
+
     elif operation == "division" or operation == "divide":
+        num1, num2 = check_num()
         division(num1, num2)
         history.append(f"{num1} / {num2} = {num1 / num2}")
-        counter = 0
 
     elif operation == "multiplication" or operation == "multiply":
+        num1, num2 = check_num()
         multiplication(num1, num2)
         history.append(f"{num1} * {num2} = {num1 * num2}")
-        counter = 0
     
     elif operation == "modulus" or operation == "remainder":
+        num1, num2 = check_num()
         modulus(num1, num2)
         history.append(f"{num1} % {num2} = {num1 % num2}")
-        counter = 0
         
-    elif operation == "history" or operation == "view history":
+    elif operation == "view history":
         print(history)
-        counter = 0
-    
+        
+    elif operation == "delete history":
+        for entry in history:
+            history.clear()
+        
     else:
-        counter += 1
+        print("Invalid operation. Please try again.")
+        continue
     
     y_n = "a"
     
