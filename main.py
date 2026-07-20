84c1da8e7e7293b1bc4a0563bb174b90ed6a8a36
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-07-20 14:46:31 -0500

    Added Functions

--- main.py
+++ main.py
@@ -6,29 +6,30 @@ num1 = int(input("Type in a number: "))
 num2 = int(input("Type in another number:"))
 
 operation_true = False
+counter = 0
 
 while operation_true == False:
-    operation = input("What operation are you going to do? (Addition, Subtraction, Multiplication, Division): ")
+    operation = input("What operation are you going to do? (Addition, Subtraction, Multiplication, Division, Modulus): ")
     operation.lower()
 
     if operation == "addition" or operation == "add":
         addition(num1, num2)
-        operation_true == True
         break
      
     elif operation == "subtraction" or operation == "subtract":
         subtraction(num1, num2)
-        operation_true == True
         break
         
     elif operation == "division" or operation == "divide":
         division(num1, num2)
-        operation_true == True
         break
 
     elif operation == "multiplication" or operation == "multiply":
         multiplication(num1, num2)
-        operation_true == True
+        break
+    
+    elif operation == "modulus" or operation == "remainder":
+        modulus(num1, num2)
         break
 
 print("This is the end of the calculator.")
--- mathfunctions.py
+++ mathfunctions.py
@@ -9,3 +9,6 @@ def division(num1, num2):
     
 def multiplication(num1, num2):
     print(f"The product of your numbers is {num1 * num2}")
+    
+def modulus(num1, num2):
+    print(f"The remainder of your numbers is {num1 % num2}.")
