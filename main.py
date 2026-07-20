85d586e0beda561b0c1c1c0433dec899c614a7df
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-07-20 17:11:28 -0500

    Added Looping and Basework for History

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from functions import *
 
 print("This is a calculator made in Python.")
 
-num1, num2 = check_num()
-
 counter = 0
 
-while True:
+history = []
+
+continue_or_not = True
+
+while continue_or_not == True:
+    
+    num1, num2 = check_num()
     
     if counter == 0:
         operation = input("What operation are you going to do? (Addition, Subtraction, Multiplication, Division, Modulus): ")
@@ -18,25 +22,43 @@ while True:
 
     if operation == "addition" or operation == "add":
         addition(num1, num2)
-        break
+        history.append(f"{num1} + {num2} = {num1 + num2}")
+        counter = 0
      
     elif operation == "subtraction" or operation == "subtract":
         subtraction(num1, num2)
-        break
+        history.append(f"{num1} + {num2} = {num1 - num2}")
+        counter = 0
         
     elif operation == "division" or operation == "divide":
         division(num1, num2)
-        break
+        history.append(f"{num1} + {num2} = {num1 / num2}")
+        counter = 0
 
     elif operation == "multiplication" or operation == "multiply":
         multiplication(num1, num2)
-        break
+        history.append(f"{num1} + {num2} = {num1 * num2}")
+        counter = 0
     
     elif operation == "modulus" or operation == "remainder":
         modulus(num1, num2)
-        break
+        history.append(f"{num1} + {num2} = {num1 % num2}")
+        counter = 0
     
     else:
         counter += 1
+    
+    y_n = "a"
+    
+    while y_n != "y" and y_n != "n":
+        y_n = input("Would you like to continue? Y/N: ")
+        y_n = y_n.lower()
+    
+    if y_n == "y":
+        continue_or_not = True
+
+    elif y_n == "n":
+        continue_or_not = False
+
 
 print("This is the end of the calculator.")
