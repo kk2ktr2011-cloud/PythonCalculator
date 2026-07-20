899d2ead5940b3b739cb931b065027d5a2761ae1
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-07-20 17:41:41 -0500

    Made History Look Better

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from functions import *
 
 print("This is a calculator made in Python.")
 
-counter = 0
+counter = 1
 
 history = []
 
@@ -16,30 +16,36 @@ while continue_or_not == True:
     if operation == "addition" or operation == "add":
         num1, num2 = check_num()
         addition(num1, num2)
-        history.append(f"{num1} + {num2} = {num1 + num2}")
+        history.append(f"{counter}. {num1} + {num2} = {num1 + num2}")
+        counter += 1
      
     elif operation == "subtraction" or operation == "subtract":
         num1, num2 = check_num()
         subtraction(num1, num2)
-        history.append(f"{num1} - {num2} = {num1 - num2}")
+        history.append(f"{counter}. {num1} - {num2} = {num1 - num2}")
+        counter += 1
 
     elif operation == "division" or operation == "divide":
         num1, num2 = check_num()
         division(num1, num2)
-        history.append(f"{num1} / {num2} = {num1 / num2}")
+        history.append(f"{counter}. {num1} / {num2} = {num1 / num2}")
+        counter += 1
 
     elif operation == "multiplication" or operation == "multiply":
         num1, num2 = check_num()
         multiplication(num1, num2)
-        history.append(f"{num1} * {num2} = {num1 * num2}")
+        history.append(f"{counter}. {num1} * {num2} = {num1 * num2}")
+        counter += 1
     
     elif operation == "modulus" or operation == "remainder":
         num1, num2 = check_num()
         modulus(num1, num2)
-        history.append(f"{num1} % {num2} = {num1 % num2}")
+        history.append(f"{counter}. {num1} % {num2} = {num1 % num2}")
+        coutner += 1
         
     elif operation == "view history":
-        print(history)
+        for entry in history:
+            print(entry)
         
     elif operation == "delete history":
         for entry in history:
